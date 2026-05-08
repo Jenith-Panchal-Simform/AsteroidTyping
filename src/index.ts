68a62699389ffd1f7510836cd8d3e5e95ec23ba0
Jenith-Panchal-Simform <jenith.panchal@simformsolutions.com>
2026-05-08 17:38:46 +0530

    fix the shoot and remove logic

--- src/index.ts
+++ src/index.ts
@@ -29,12 +29,12 @@ let randomArray: string[] = [
 //asteroid type
 type asteroid = {
     image: HTMLElement;
-    span:HTMLElement
+    span: HTMLElement;
     x: number;
     y: number;
     velocityX: number;
     velocityY: number;
-};
+} | undefined;
 //initializations
 let lives = 3;
 let score = 0;
@@ -42,20 +42,17 @@ let gameBodyTop: number;
 let gameBodyWidth: number;
 let gameBodyBottom: number;
 
-let liveContainer= document.getElementById(
-    "lives"
-)as HTMLElement
+let liveContainer = document.getElementById("lives") as HTMLElement;
 
-let scoreContainer = document.getElementById(
-    "score"
-)as HTMLElement ;
+let scoreContainer = document.getElementById("score") as HTMLElement;
 let asteroidHeight = 50;
 let asterroidWidth = 50;
 let asteroids: asteroid[] = [];
 
-const inputBox = document.getElementById("input") as HTMLInputElement;
+let inputBox = document.getElementById("input") as HTMLInputElement;
+let resetBtn=document.getElementById("reset") as HTMLButtonElement
 
-let gameBody  = document.getElementById("game-body") as HTMLElement ;
+let gameBody = document.getElementById("game-body") as HTMLElement;
 if (gameBody instanceof HTMLElement) {
     gameBodyTop = gameBody.offsetTop;
     gameBodyWidth = gameBody.offsetWidth;
@@ -77,7 +74,7 @@ function shuffleArray(arr: string[]): string[] {
     return arr;
 }
 
-//onload 
+//onload
 window.onload = function () {
     startGame();
     setInterval(() => {
@@ -86,6 +83,7 @@ window.onload = function () {
 };
 let arr: string[] = shuffleArray(randomArray);
 
+
 //start the game
 function startGame() {
     liveContainer.innerText = String(lives);
@@ -93,11 +91,11 @@ function startGame() {
     //drop images from Top
     for (let i = 0; i < 4; i++) {
         //<div><img id="arr[0]" src="image"><span>arra[0]<span></div>
-        let asteroidContainer:HTMLDivElement=document.createElement("div")
-        let asteroidSpan:HTMLSpanElement=document.createElement("span")
-        if(asteroidSpan)
-        {
-            asteroidSpan.innerText=String(arr[i]);
+        let asteroidContainer: HTMLDivElement = document.createElement("div");
+        let asteroidSpan: HTMLSpanElement = document.createElement("span");
+        if (asteroidSpan) {
+            asteroidSpan.innerText = String(arr[i]);
+            asteroidSpan.classList.add("asteroid-span")
         }
         let asteroid: HTMLImageElement = document.createElement("img");
         asteroid.src = "./assets/asteroid.png";
@@ -105,11 +103,11 @@ function startGame() {
         asteroid.width = asterroidWidth;
         asteroidContainer.classList.add("asteroid");
 
-        asteroidContainer.style.background="./assets/asteroid.png"
-        asteroidContainer.style.width=asterroidWidth+"px"
-        asteroidContainer.style.height=asteroidHeight+"px"
-        asteroidContainer.appendChild(asteroid)
-        asteroidContainer.appendChild(asteroidSpan)
+        asteroidContainer.style.background = "./assets/asteroid.png";
+        asteroidContainer.style.width = asterroidWidth + "px";
+        asteroidContainer.style.height = asteroidHeight + "px";
+        asteroidContainer.appendChild(asteroid);
+        asteroidContainer.appendChild(asteroidSpan);
         if (gameBody instanceof HTMLElement) {
             gameBody.appendChild(asteroidContainer);
         }
@@ -121,7 +119,7 @@ function startGame() {
             velocityX: randomPosition(2),
             velocityY: randomPosition(1),
         };
-    
+
         asteroids.push(asteroidObj);
     }
 }
@@ -133,57 +131,85 @@ function moveAsteroid() {
     for (let i = 0; i < asteroids.length; i++) {
         let asteroidContainer = asteroids[i];
         if (asteroidContainer) {
-            asteroidContainer.x =asteroidContainer.x
             asteroidContainer.y += asteroidContainer.velocityY;
 
             asteroidContainer.image.style.transform = `translate3d(${asteroidContainer.x}px, ${asteroidContainer.y}px, 0)`;
 
-            if ((asteroidContainer.y + asteroidHeight)> gameBodyBottom) {
-                asteroids = asteroids.filter(item => item !== asteroidContainer);
+            if ((asteroidContainer.y + asteroidHeight) > gameBodyBottom) {
+                asteroids = asteroids.filter(
+                    (item) => item !== asteroidContainer,
+                );
                 asteroidContainer.image.style.display = "none";
-                console.log(asteroids)
-                if (lives <= 0) {
-                    // console.log("hello")
-                    // alert("Your score:",score)
-                    // resetGame()
-                }
                 lives--;
                 liveContainer.innerText = String(lives);
+                if (lives <= 0 ||  asteroids.length<=0) {
+                    resetGame()
+                }
             }
         }
     }
 }
-
+let asteroidFind:asteroid;
 if (inputBox) {
-  inputBox.addEventListener("keydown", (e) => {
-    const typed = e.key;
-    // Iterate over asteroids safely 
-    asteroids.slice().forEach((obj, index) => {
-        const currentText = obj.span.innerText;
-      // Case 1: span starts with typed string
-      if (currentText.startsWith(typed) && typed.length > 0) {
-        // obj.span.innerText = `Match: ${currentText}`;
-        obj.span.innerText=processStrings(currentText,typed)
-      }
-      // Case 2: span equals typed string
-      if (currentText === typed) {
-        // Remove from DOM
-        obj.image.remove();
-        // Remove from array
-        asteroids.splice(index, 1);
-        //increase Score
-        score++
-        scoreContainer.innerText = String(score);
-        inputBox.value=""
-      }
-    });
+    inputBox.addEventListener("keydown", (e) => {
+        const typed = e.key;
+        if(!asteroidFind)
+        {
+            asteroidFind = asteroids.find((obj) => {
+                const currentText = obj?.span.innerText[0];
+                if (typed == currentText)
+                return obj;
+            }) as asteroid;
+        }
+        if(asteroidFind)
+        {
 
-  })
+            const currentText = asteroidFind?.span.innerText;
+            // Case 1: span starts with typed string
+            if (currentText?.startsWith(typed) && typed.length > 0) {
+                asteroidFind.span.innerText = processStrings(
+                    currentText,
+                    typed,
+                );
+            }
+            // Case 2: span equals typed string
+            if (currentText === typed) {
+                // Remove from DOM
+                asteroidFind.image.remove();
+                // Remove from array
+                asteroids = asteroids.filter(
+                    (item) => item !== asteroidFind,
+                );
+                asteroidFind=undefined;
+                //increase Score
+                score++;
+                scoreContainer.innerText = String(score);
+                inputBox.value = "";
+            }
+        }
+    });
 }
-function processStrings(str1:string, str2:string) {
+function processStrings(str1: string, str2: string) {
     // If str1 starts with str2, return the remaining part
     if (str1.startsWith(str2)) {
         return str1.slice(str2.length);
     }
     return str2;
 }
+
+
+//reset handler
+function resetGame()
+{
+    let el=confirm("Your score is:"+score+" Do You want to paly again?");
+    score=0;
+    scoreContainer.innerText=String(0);
+
+    lives=0
+    liveContainer.innerText=String(0)
+    if(el)
+    {
+        location.reload()
+    }
+}
+resetBtn.addEventListener("click",resetGame)
